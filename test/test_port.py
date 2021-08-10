d71894fbb2a2c6f3e45208044a682f37997e6044
xUborka <xgurke@gmail.com>
2021-08-10 12:11:02 +0100

    Init pytestqt

--- test/test_port.py
+++ test/test_port.py
@@ -3,9 +3,9 @@ from Port import Port
 from pyTreadmill import Window
 
 
-def test_empty_init():
-    pass
-#     dummy_window = Window()
-#     qtbot.addWidget(dummy_window)
-#     # dummy_port = Port('A', None, None, None)
-#     assert True
+def test_empty_init(qtbot):
+    # pass
+    dummy_window = Window()
+    qtbot.addWidget(dummy_window)
+    # dummy_port = Port('A', None, None, None)
+    assert True
