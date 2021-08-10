abd90d659e10a0478614acb77f0c2887ac53e054
xUborka <xgurke@gmail.com>
2021-08-10 11:52:02 +0100

    Init pytestqt

--- test/test_port.py
+++ test/test_port.py
@@ -6,6 +6,5 @@ from pyTreadmill import Window
 def test_empty_init(qtbot):
     # pass
     dummy_window = Window()
-    dummy_window.show()
     qtbot.addWidget(dummy_window)
     # dummy_port = Port('A', None, None, None)
