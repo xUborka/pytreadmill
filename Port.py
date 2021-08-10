e8169e04329fda79cfd53ddd2f688e6c5bd3b519
xUborka <xgurke@gmail.com>
2021-08-10 09:56:57 +0100

    Position Trigger Extracted

--- Port.py
+++ Port.py
@@ -1,6 +1,7 @@
-from PyQt5.QtCore import Qt, QThread, pyqtSignal, QTimer, QObject
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QTimer
 from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QLabel,\
     QSpinBox, QGroupBox
+from positionTrigger import PositionTriggerData, PositionTriggerWorker
 
 
 class Port(QWidget):
@@ -177,74 +178,3 @@ class Port(QWidget):
     def enableChildrenWidgets(self, object):
         for child in object.findChildren(QWidget):
             child.setEnabled(True)
-
-
-class PositionTriggerData:
-    def __init__(self, port):
-        self.port = port
-        self.thread: QThread
-        self.isActive = False
-
-        self.start: int
-        self.window: int
-        self.retention: int
-        self.duration: int
-
-
-class PositionTriggerWorker(QObject):
-    triggerSignal = pyqtSignal()
-    finished = pyqtSignal()
-    checkerInterval = 50
-
-    def __init__(self, positionTriggerData, parent=None):
-        super(PositionTriggerWorker, self).__init__(parent)
-
-        self.positionTriggerData = positionTriggerData
-        self.getTreadmillData = positionTriggerData.port.getTreadmillData
-
-        self.isRunning = True
-        self.isSingleShot = True
-        self.hasFired = False
-
-        self.triggerTimer = QTimer(self)
-        self.triggerTimer.setSingleShot(False)
-        self.triggerTimer.timeout.connect(self.trigger)
-
-        self.checkerTimer = QTimer(self)
-        self.checkerTimer.setSingleShot(False)
-        self.checkerTimer.timeout.connect(self.checkPosition)
-
-    def process(self):
-        self.hasFired = False
-        self.checkerTimer.start(self.checkerInterval)
-        self.triggerTimer.start(self.positionTriggerData.retention)
-
-    def trigger(self):
-        if self.isSingleShot:
-            if not self.hasFired:
-                self.triggerSignal.emit()
-                # print("triggerTimer fired")
-        else:
-            self.triggerSignal.emit()
-            # print("triggerTimer fired")
-        self.hasFired = True
-
-    def setTimerSingleShot(self, isSingleShot):
-        # self.triggerTimer.setInterval(self.positionTriggerData.retention)
-        self.isSingleShot = isSingleShot
-
-    def updateTriggerInterval(self):
-        self.triggerTimer.setInterval(self.positionTriggerData.retention)
-
-    def checkPosition(self):
-        treadmillData = self.getTreadmillData()
-        if treadmillData.relPosition < self.positionTriggerData.start \
-                or treadmillData.relPosition > (self.positionTriggerData.start + self.positionTriggerData.window):
-            self.terminate()
-
-    def terminate(self):
-        self.isRunning = False
-        self.hasFired = False
-        self.checkerTimer.stop()
-        self.triggerTimer.stop()
-        self.finished.emit()
---
+++ positionTrigger.py
@@ -0,0 +1,72 @@
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer, QObject
+
+
+class PositionTriggerData:
+    def __init__(self, port):
+        self.port = port
+        self.thread: QThread
+        self.isActive = False
+
+        self.start: int
+        self.window: int
+        self.retention: int
+        self.duration: int
+
+
+class PositionTriggerWorker(QObject):
+    triggerSignal = pyqtSignal()
+    finished = pyqtSignal()
+    checkerInterval = 50
+
+    def __init__(self, positionTriggerData, parent=None):
+        super(PositionTriggerWorker, self).__init__(parent)
+
+        self.positionTriggerData = positionTriggerData
+        self.getTreadmillData = positionTriggerData.port.getTreadmillData
+
+        self.isRunning = True
+        self.isSingleShot = True
+        self.hasFired = False
+
+        self.triggerTimer = QTimer(self)
+        self.triggerTimer.setSingleShot(False)
+        self.triggerTimer.timeout.connect(self.trigger)
+
+        self.checkerTimer = QTimer(self)
+        self.checkerTimer.setSingleShot(False)
+        self.checkerTimer.timeout.connect(self.checkPosition)
+
+    def process(self):
+        self.hasFired = False
+        self.checkerTimer.start(self.checkerInterval)
+        self.triggerTimer.start(self.positionTriggerData.retention)
+
+    def trigger(self):
+        if self.isSingleShot:
+            if not self.hasFired:
+                self.triggerSignal.emit()
+                # print("triggerTimer fired")
+        else:
+            self.triggerSignal.emit()
+            # print("triggerTimer fired")
+        self.hasFired = True
+
+    def setTimerSingleShot(self, isSingleShot):
+        # self.triggerTimer.setInterval(self.positionTriggerData.retention)
+        self.isSingleShot = isSingleShot
+
+    def updateTriggerInterval(self):
+        self.triggerTimer.setInterval(self.positionTriggerData.retention)
+
+    def checkPosition(self):
+        treadmillData = self.getTreadmillData()
+        if treadmillData.relPosition < self.positionTriggerData.start \
+                or treadmillData.relPosition > (self.positionTriggerData.start + self.positionTriggerData.window):
+            self.terminate()
+
+    def terminate(self):
+        self.isRunning = False
+        self.hasFired = False
+        self.checkerTimer.stop()
+        self.triggerTimer.stop()
+        self.finished.emit()
